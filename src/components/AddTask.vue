4717357a1a3268436fc96cb1ec191e34976a23a5
alikarimi <karimi.me.ali@gmail.com>
2022-12-11 20:45:07 +0330

    add v-model and style

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,19 +1,20 @@
 <template>
-    <form
-        class="add-form"
-    >
+    <form class="add-form">
         <div class="form-control">
             <label>Task</label>
             <input
                 type="text"
+                v-model="text"
                 name="text"
                 placeholder="Add Task"
             />
         </div>
+        {{text}}  <!-- text will show up here as you type -->
         <div class="form-control">
             <label>Day & Time</label>
             <input
                 type="text"
+                v-model="day"
                 name="day"
                 placeholder="Add Day & Time"
             />
@@ -22,6 +23,7 @@
             <label>Set Reminder</label>
             <input
                 type="checkbox"
+                v-model="reminder"
                 name="reminder"
             />
         </div>
@@ -37,8 +39,49 @@
 <script>
 export default {
     name: "AddTask",
+    data() {
+        return {
+            text: "default",  // default value
+            day: "",
+            reminder: false,
+        };
+    },
 };
 </script>
 
-<style>
+<style scoped>
+.add-form {
+    margin-bottom: 40px;
+}
+
+.form-control {
+    margin: 20px 0;
+}
+
+.form-control label {
+    display: block;
+}
+
+.form-control input {
+    width: 100%;
+    height: 40px;
+    margin: 5px;
+    padding: 3px 7px;
+    font-size: 17px;
+}
+
+.form-control-check {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.form-control-check label {
+    flex: 1;
+}
+
+.form-control-check input {
+    flex: 2;
+    height: 20px;
+}
 </style>
